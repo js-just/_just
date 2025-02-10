f26ebeb7c77c9f3045fef3032aeaec8eda1867b1
JustDeveloper <justdeveloper@juststudio.is-a.dev>
2025-02-10 23:19:11 +0300

    Update build_map.sh

--- src/build_map.sh
+++ src/build_map.sh
@@ -31,36 +31,74 @@ echo "$(cat $GITHUB_ACTION_PATH/src/buildManifest_start.js)" > deploy/_just/stat
 echo -e "\n----------------\n\nBuild Map:\n"
 FILE_ID=1
 TOTAL_FILES=$(find deploy -mindepth 1 -print | wc -l)
+TOTAL_SIZE=0
 find deploy -mindepth 1 -print | while read -r path; do
     relative_path=${path#deploy/}
-
-    if [ "$FILE_ID" -eq 1 ]; then
-        echo "┌ $relative_path"
-    elif [ "$FILE_ID" -eq "$TOTAL_FILES" ]; then
-        echo -e "└ $relative_path\n"
-    else
-        echo "├ $relative_path"
-    fi
     
     if [ -f "$path" ]; then
         case "${path##*.}" in
             html) type="HTML" ;;
+            php) type="PHP" ;;
             css) type="CSS" ;;
             js) type="JavaScript" ;;
+            json) type="JSON" ;;
             txt) type="Text" ;;
+            xml) type="XML" ;;
+            webmanifest) type="Webmanifest" ;;
+
             png) type="Image/Png" ;;
-            svg) type="Image/Svg" ;;
+            svg) type="Image/SVG" ;;
             jpeg|jpg) type="Image/Jpeg" ;;
+            webp) type="Image/Webp" ;;
+            bmp) type="Image/Bmp" ;;
+            gif) type="Image/GIF" ;;
+            heic) type="Image/HEIC" ;;
+            ico) type="Favicon" ;;
+
+            mp4) type="Video/MP4" ;;
+            mov) type="Video/Mov" ;;
+            webm) type="Video/Webm" ;;
+
+            otf) type="Font/OTF" ;;
+            woff) type="Font/WOFF" ;;
+            woff2) type="Font/WOFF2" ;;
+
+            m4a) type="Audio/M4A" ;;
+            mp2) type="Audio/MP2" ;;
+            mp3) type="Audio/MP3" ;;
+            ogg) type="Audio/OGG" ;;
+            wav) type="Audio/WAV" ;;
+            wma) type="Audio/WMA" ;;
+
+            7z) type="Archive/7z" ;;
+            tar) type="Archive/TAR" ;;
+            rar) type="Archive/RAR" ;;
+            zip) type="Archive/ZIP" ;;
+            
             *) type="Other" ;;
         esac
-        echo "    _just_buildManifest.push({\"type\": \"$type\", \"path\": \"$relative_path\"});" >> deploy/_just/static/$BUILD_ID/buildManifest.js
+        file_size=$(stat -c%s "$path")
+        TOTAL_SIZE=$((TOTAL_SIZE + $file_size))
+            if [ "$FILE_ID" -eq 1 ]; then
+                printf "┌ %3d B | %s\n" "$file_size" "$relative_path"
+            elif [ "$FILE_ID" -eq "$TOTAL_FILES" ]; then
+                printf "└ %3d B | %s\n" "$file_size" "$relative_path"
+            else
+                printf "├ %3d B | %s\n" "$file_size" "$relative_path"
+            fi
+        echo "    _just_buildManifest.push({\"type\": \"$type\", \"path\": \"$relative_path\", \"size\": $file_size});" >> deploy/_just/static/$BUILD_ID/buildManifest.js
     fi
     
     FILE_ID=$((FILE_ID + 1))
 done
-echo -e "End Build Map\n"
+
+manifest_size=$(stat -c%s "deploy/_just/static/$BUILD_ID/buildManifest.js")
+echo -e "End Build Map\n\n"
+echo -e "_just/static/$BUILD_ID/buildManifest.js size: $manifest_size bytes\n"
+echo -e "                            Total build size: $TOTAL_SIZE bytes\n\n"
 echo -e "----------------\n"
 echo "$(cat $GITHUB_ACTION_PATH/src/buildManifest_end.js)" >> deploy/_just/static/$BUILD_ID/buildManifest.js
+
 # Override Deployment
 for html_file in deploy/*.html; do
   echo "<script src=\"_just/static/$BUILD_ID/buildManifest.js\"></script>" >> "$html_file"
