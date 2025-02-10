7c6afe210c0ae0c3a4ca88bd2529b6aa94bca157
JustDeveloper <justdeveloper@juststudio.is-a.dev>
2025-02-10 22:48:16 +0300

    Update build_map.sh

--- src/build_map.sh
+++ src/build_map.sh
@@ -26,7 +26,6 @@ BUILD_ID=$(cat .next/BUILD_ID)
 mkdir -p deploy/_just/static/
 mkdir -p deploy/_just/static/$BUILD_ID/
 
-# echo "const _just_buildManifest = [];" > deploy/_just/static/$BUILD_ID/buildManifest.js
 echo "$(cat $GITHUB_ACTION_PATH/src/buildManifest_start.js)" > deploy/_just/static/$BUILD_ID/buildManifest.js
 
 echo -e "\n----------------\n\nBuild Map:\n"
@@ -61,7 +60,8 @@ find deploy -mindepth 1 -print | while read -r path; do
 done
 echo -e "End Build Map\n"
 echo -e "----------------\n"
-echo "window._just_Manifest = _just_buildManifest;" >> deploy/_just/static/$BUILD_ID/buildManifest.js
+# echo "window._just_Manifest = _just_buildManifest;" >> deploy/_just/static/$BUILD_ID/buildManifest.js
+echo "$(cat $GITHUB_ACTION_PATH/src/buildManifest_end.js)" >> deploy/_just/static/$BUILD_ID/buildManifest.js
 # Override Deployment
 for html_file in deploy/*.html; do
   echo "<script src=\"_just/static/$BUILD_ID/buildManifest.js\"></script>" >> "$html_file"
