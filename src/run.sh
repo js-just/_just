a6ed1d87b489c2e62c06e8cd7aefb95376515859
JustDeveloper <176615419+JustDeveloper1@users.noreply.github.com>
2025-05-11 23:08:39 +0500

    Update run.sh

--- src/run.sh
+++ src/run.sh
@@ -135,6 +135,7 @@ elif [ "$TYPE" == "generator" ]; then
     mkdir -p deploy && \
     installNodejs && \
     echo "$msg7" && \
+    npm cache clean --force && \
     npm install jsdom > /dev/null 2>&1 && \
     echo "$msg8" && \
     bash $GITHUB_ACTION_PATH/src/documentation/checks.sh && \
