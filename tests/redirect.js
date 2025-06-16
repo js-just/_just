b196a0ae0d8a466b74d3bae4cc20a1541102435d
JustDeveloper <176615419+JustDeveloper1@users.noreply.github.com>
2025-06-16 14:26:03 +0500

    add redirect ci

---
+++ tests/redirect.js
@@ -0,0 +1,11 @@
+module.exports = {
+    type: "redirect", 
+    redirect_config: {
+        url: "https://juststudio.is-a.dev/", 
+        params: {
+            title: "JustStudio.",
+            description: "a System Development Studio",
+            htmlLang: "en"
+        }
+    }
+}
