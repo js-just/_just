fe1843fddf208985f7f942b0d3c69986c743c401
JustDeveloper <176615419+JustDeveloper1@users.noreply.github.com>
2025-06-21 20:22:55 +0500

    Update run.sh

--- src/run.sh
+++ src/run.sh
@@ -183,7 +183,7 @@ elif [ "$TYPE" == "docs" ]; then
     bash $GITHUB_ACTION_PATH/src/documentation/checks.sh && \
     INDEXJS0="$GITHUB_ACTION_PATH/src/documentation/index.js"
     INDEXJS1=$(cat "$INDEXJS0") && \
-    INDEXJS2="let [text] = process.argv.slice(2);text = text.split('\n');for (let i = 0; i < text.length; i++) {text[i] = text[i].replaceAll('(__REPLACE_LINE__)',`(\${i+1})`)};console.log(text.join('\n'))" && \
+    INDEXJS2="let [text] = process.argv.slice(2);text = text.split('\n');for (let i = 0; i < text.length; i++) {text[i] = text[i].replaceAll('(__REPLACE_LINE__)',\`(\${i+1})\`)};console.log(text.join('\n'))" && \
     echo "$INDEXJS2" && \
     echo "$INDEXJS2" > "$INDEXJS0" && \
     INDEXJS3=$(node "$INDEXJS0" "$INDEXJS1") && \
