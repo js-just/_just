d01e277fd72de5954b5df2a5f25592350aa472cc
JustDeveloper <176615419+JustDeveloper1@users.noreply.github.com>
2025-06-22 20:44:17 +0500

    test
    fake tld

--- tests/just.config.js
+++ tests/just.config.js
@@ -2,7 +2,7 @@ module.exports = {
     type: "docs",
     docs_config: {
         metatitle: "Just an Ultimate Site Tool",
-        domain: "test.just.is-a.dev",
+        domain: "test.just.is-a.dev.faketld",
         links: [
             ["a link", "https://juststudio.is-a.dev/", "_blank"],
             ["another link", "https://justdeveloper.is-a.dev/", "_blank"]
