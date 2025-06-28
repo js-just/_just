fe04d28d4bdb7e621d6b4b2bef5b7ca261b8e960
JustDeveloper <176615419+JustDeveloper1@users.noreply.github.com>
2025-06-28 20:56:57 +0500

    Update errmsg.js

--- src/modules/errmsg.js
+++ src/modules/errmsg.js
@@ -30,7 +30,9 @@ exports.errormessage = function (code, message, type = 'Error') {
     return new Promise((resolve, reject) => {
         exec(`bash -c 'source $GITHUB_ACTION_PATH/src/modules/errmsg.sh && mkdir _just_data && echo "$(customErrorMessage "${type}" "${code}" "${message}")" > "_just_data/e.txt" && echo -e $(customErrorMessage "${type}" "${code}" "${message}")'`, (error, stdout, stderr) => {
         if (error) {
-            reject(stderr);
+            exec(`bash -c 'source $GITHUB_ACTION_PATH/src/modules/errmsg.sh && echo -e "$(customErrorMessage "${type}" "${code}" "${message}")"`, (err, out, outerr) => {
+                resolve(out);
+            });
         } else {
             resolve(stdout);
         }
