b31cf68d8d363520da307d6d3ad47dca1910e06c
JustDeveloper <176615419+JustDeveloper1@users.noreply.github.com>
2025-06-29 12:43:19 +0500

    updates

--- src/documentation/index.js
+++ src/documentation/index.js
@@ -452,7 +452,7 @@ checkTLD(domain).then(tldvalid => {
                 .replace(/(?<=\s|^)([-+*])\s\[( {0,}x {0,}| {0,}X {0,}| {1,})\]\s(.*?)(?=\s|\n|$)/g, (match, prefix, type_, text_) => {
                     const isChecked = type_.trim().toLowerCase() === 'x';
                     const checkedAttr = isChecked ? ' checked' : '';
-                    return `<input type="checkbox" disabled${checkedAttr}> ${text_.trim()}`;
+                    return `<input type="checkbox" ${checkedAttr}> ${text_.trim()}`;
                 });
         return _just.MDtoHTML.MDtoHTML(text, cssclass).replace(/~(.*?)~/g, '<sub>$1</sub>').replace(/\^(.*?)\^/g, '<sup>$1</sup>').replace(/\*\*(.*?)\*\*/g, '<strong>$1</strong>').replace(/\*(.*?)\*/g, '<em>$1</em>');
     }
