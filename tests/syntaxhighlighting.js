adbb82a33b89b4ba04f349ac79a424898139469b
JustDeveloper <176615419+JustDeveloper1@users.noreply.github.com>
2025-07-14 17:25:03 +0500

    (tests) Syntax highlighting test

---
+++ tests/syntaxhighlighting.js
@@ -0,0 +1,47 @@
+/*
+
+MIT License
+
+Copyright (c) 2025 JustStudio. <https://juststudio.is-a.dev/>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+*/
+
+const fs = require('fs');
+const path = require('path');
+
+function findFiles(dir) {
+    let output = '# test\n';
+    const list = fs.readdirSync(dir);
+    list.forEach(file => {
+        file = path.join(dir, file);
+        const stat = fs.statSync(file);
+        if (stat && stat.isDirectory()) {
+            output += findFiles(file);
+            console.log('Dir:  '+file);
+        } else if (!file.endsWith('.md') && !file.endsWith('.mdx')) {
+            output += `\`\`\`${path.extname(file)}\n${fs.readFileSync(file)}\n\`\`\`\n`;
+            console.log('File: '+file);
+        }
+    });
+    return output;
+}
+
+fs.writeFileSync(path.join('.', 'website/syntax-highlighting.md'), findFiles(process.cwd()));
