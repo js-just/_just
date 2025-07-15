db3d0f2773152522316585ca200ff96186ef3004
JustDeveloper <176615419+JustDeveloper1@users.noreply.github.com>
2025-07-15 22:25:26 +0500

    Update syntaxhighlighting.js

--- tests/syntaxhighlighting.js
+++ tests/syntaxhighlighting.js
@@ -37,7 +37,7 @@ function findFiles(dir) {
             output += findFiles(file);
             console.log('Dir:  '+file);
         } else if (/*!file.endsWith('.md') &&*/ !file.endsWith('.mdx') && !file.endsWith('.idx') && !file.endsWith('.pack') && !file.endsWith('highlight.js') && !file.endsWith('highlight.min.js') && !file.endsWith('mdtohtml.js') && !file.endsWith('documentation/index.js') && path.extname(file) && ( file.startsWith('/home/runner/work/_just/_just/src') || file.startsWith('/home/runner/work/_just/_just/website') )) {
-            output += `\`\`\`${path.extname(file).replace('.','')}\n${fs.readFileSync(file)}\n\`\`\`\n`;
+            output += `\`\`\`${path.extname(file).replace('.','')}\n${file.endsWith('.md') ? fs.readFileSync(file).replaceAll("```", "\\`\\`\\`") : fs.readFileSync(file)}\n\`\`\`\n`;
             console.log('File: '+file);
         }
     });
