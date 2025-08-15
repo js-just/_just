cd24e7229fbc119580d4e4db5675ef2da48cc719
JustDeveloper <176615419+JustDeveloper1@users.noreply.github.com>
2025-08-15 23:16:42 +0500

    unts😭

--- src/run.sh
+++ src/run.sh
@@ -233,8 +233,6 @@ elif [ "$TYPE" == "docs" ]; then
     mkdir -p _just && \
     mkdir -p deploy && \
     installNodejs && \
-    installTypeScriptCompiler && \
-    tsc lib/array.ts && \
     bash $GITHUB_ACTION_PATH/src/documentation/checks.sh && \
     INDEXJS0="$GITHUB_ACTION_PATH/src/documentation/index.js"
     INDEXJS1=$(cat "$INDEXJS0") && \
