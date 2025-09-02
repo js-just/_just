8a1aee9338fdcdfa691aae44198e1606160fb90c
JustDeveloper <176615419+JustDeveloper1@users.noreply.github.com>
2025-09-02 18:07:08 +0500

    (website) Fix

--- just.config.js
+++ just.config.js
@@ -11,7 +11,7 @@ module.exports = {
             <link rel="apple-touch-icon" sizes="180x180" href="/img/apple-touch-icon.png">
             <link rel="icon" type="image/png" sizes="32x32" href="/img/favicon-32x32.png">
             <link rel="icon" type="image/png" sizes="16x16" href="/img/favicon-16x16.png">
-            <link rel="manifest" href="/site.webmanifest"></link>
+            <link rel="manifest" href="/site.webmanifest">
             <script src="/js/f.js"></script>
         `,
         footer: '<span onclick="javascript:window.open(\'https://github.com/js-just/_just/blob/main/LICENSE\',\'\_blank\')">Copyright &copy; 2025 &#171;<a href="https://juststudio.is-a.dev/" target="_blank">JustStudio.</a>&#187;</span>',
