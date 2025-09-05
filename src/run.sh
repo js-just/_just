a8cfc733e8cf803f8ee65e4cfbeec9929de1d000
JustDeveloper <176615419+JustDeveloper1@users.noreply.github.com>
2025-09-05 21:41:09 +0500

    Update run.sh

--- src/run.sh
+++ src/run.sh
@@ -169,7 +169,11 @@ if [[ "${USESASS,,}" == "true" ]]; then
         ERROR_MESSAGE=$(ErrorMessage "important_dirs" "0106")
         echo -e "::error::$ERROR_MESSAGE" && exit 1
     fi
-    curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh | sudo -E bash - > /dev/null 2>&1
+    
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> ~/.bashrc
+    eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+
     brew install sass/sass/sass
 fi
 
