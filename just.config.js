add113be915c676b03f64d12d3e6bc5f829816a8
JustDeveloper <176615419+JustDeveloper1@users.noreply.github.com>
2025-09-08 20:25:15 +0300

    test

--- just.config.js
+++ just.config.js
@@ -1,16 +1,18 @@
+const i = '/img/';
+
 module.exports = {
     type: "docs",
     docs_config: {
         metatitle: "Just an Ultimate Site Tool",
         domain: "just.js.org",
-        logo: "/img/logo.svg",
+        logo: `${i}logo.svg`,
         links: [
             ["Documentation", "/docs", "_self"],
         ],
         insertInHTMLHead: `
-            <link rel="apple-touch-icon" sizes="180x180" href="/img/apple-touch-icon.png">
-            <link rel="icon" type="image/png" sizes="32x32" href="/img/favicon-32x32.png">
-            <link rel="icon" type="image/png" sizes="16x16" href="/img/favicon-16x16.png">
+            <link rel="apple-touch-icon" sizes="180x180" href="${i}apple-touch-icon.png">
+            <link rel="icon" type="image/png" sizes="32x32" href="${i}favicon-32x32.png">
+            <link rel="icon" type="image/png" sizes="16x16" href="${i}favicon-16x16.png">
             <link rel="manifest" href="/site.webmanifest">
             <script src="/js/f.js"></script>
         `,
