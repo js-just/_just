efbbeb0c5e879478f23ded8f75099c1b238a3f3f
JustDeveloper <176615419+JustDeveloper1@users.noreply.github.com>
2025-10-29 23:24:54 +0500

    Update run.sh

--- src/run.sh
+++ src/run.sh
@@ -222,7 +222,7 @@ CONFIG_VALUES=$(echo "$CONFIG_JSON" | jq -r '
 .compile.sass,
 .compile.scss,
 .install.uglifyjs,
-(.uglifyjs.reserve // []) | tojson,
+(.uglifyjs?.reserve // []) | tojson,
 .uglifyjs.unsafe,
 .uglifyjs.source_map,
 .uglifyjs.disable.dead_code,
