01d3a188864324a030994c3c54a02f6f4ac9bbb6
JustDeveloper <176615419+JustDeveloper1@users.noreply.github.com>
2025-10-30 01:38:44 +0500

    Update run.sh

--- src/run.sh
+++ src/run.sh
@@ -384,9 +384,12 @@ mode_compressor() {
             local args=("$js_file")
             local compress_opts=()
             if [ "$UGLIFYJS_R" != "[]" ]; then
-                echo "::warning::$UGLIFYJS_R"
-                local UGLIFYJS_R_ARG=$(echo "$UGLIFYJS_R" | tr -d '[]' | tr ',' ' ' || echo "")
-                args+=(-m "reserved=[$UGLIFYJS_R_ARG]")
+                local UGLIFYJS_R_ARG=$(echo "$UGLIFYJS_R" | jq -r '.[]' | tr '\n' ',' | sed 's/,$//')
+                if [ -n "$UGLIFYJS_R_ARG" ]; then
+                    args+=(-m "reserved=[$UGLIFYJS_R_ARG]")
+                else
+                    args+=(-m)
+                fi
             else
                 args+=(-m)
             fi
