5a715edb53c450ba3ff1a5c7b15bc76985c1c95f
JustDeveloper <176615419+JustDeveloper1@users.noreply.github.com>
2025-10-30 23:02:25 +0500

    Update run.sh

--- src/run.sh
+++ src/run.sh
@@ -405,7 +405,7 @@ mode_compressor() {
                 args+=(--source-map)
             fi
 
-            echo "::warning::${args[@]}"
+            echo "::debug::output command:   npx uglify-js@3 ${args[@]}"
 
             local UGLIFYJS_OUTPUT=$(npx uglify-js@3 "${args[@]}") # 2>&1 >/dev/null)
             if [ $? -ne 0 ]; then
